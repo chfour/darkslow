2bacbada0e04f43f84614cceb8384f0e2c3a8f9e
chfour <michaleklitwin@gmail.com>
2022-08-31 21:13:48 +0200

    parse events

--- main.js
+++ main.js
@@ -18,7 +18,31 @@ console.debug("loaded", window.location.hash);
     const streamData = await infoRequest.json();
     console.debug("streamdata", streamData);
     addToLog(`user id: ${streamData._id}`);
+
     const events = new WebSocket(`wss://events.lightspeed.tv/?channel=${streamData._id}`);
+    events.onmessage = e => {
+        const message = JSON.parse(e.data);
+        console.debug(message);
+        switch (message.type) {
+            case "ChatMessage":
+                addToLog(`<${message.message.author.username}> ${message.message.content}`);
+                break;
+            case "StreamStart":
+                addToLog("stream start");
+                break;
+            case "StreamEnd":
+                addToLog("stream end");
+                break;
+            case "Follow":
+                addToLog(`${message.user_id} followed`);
+                break;
+            case "Unfollow":
+                addToLog(`${message.user_id} unfollowed`);
+                break;
+            default:
+                addToLog("// unknown: " + JSON.dumps(message));
+        }
+    }
     events.onopen = e => {addToLog("connected to events!")};
 
 })();
